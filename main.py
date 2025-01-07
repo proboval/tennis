c26775e1efb460776d1aabd559284ae36aee4b72
Saveliy <chertkov.03@inbox.ru>
2025-01-07 13:41:01 +0300

    start dev

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from game import Game
+
 
 if __name__ == '__main__':
-    print('test')
+    tennis = Game()
+
+    flag = -1
+    while flag == -1:
+        flag = tennis.start()
+
+    tennis.set_length()
+    tennis.start_game()
